3622a89f2a189ee0395d49053ceecb37a07dd8c9
= <jonathanl@codev.com>
2025-06-09 04:00:44 +0800

    - Ui Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Dropdown } from 'primereact/dropdown';
+import { InputText } from 'primereact/inputtext';
+import { Divider } from 'primereact/divider';
+import { Fieldset } from 'primereact/fieldset';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +24,27 @@ const styles = {
     padding: 20,
     fontFamily: 'Arial, sans-serif',
   },
-  title: {
-    textAlign: 'center',
-    marginBottom: 20,
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  settingsButton: {
+    fontSize: 20,
+    background: 'none',
+    border: 'none',
+    cursor: 'pointer',
+  },
+  settingsPanel: {
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 10,
+  },
+  select: {
+    marginLeft: 10,
+    padding: 5,
+    fontSize: 14,
   },
   chatBox: {
     display: 'flex',
@@ -61,12 +83,26 @@ const styles = {
     borderRadius: 8,
     cursor: 'pointer',
   },
+  selection: {
+    paddingTop: '30px !important'
+  }
 };
 
 export default function Home() {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
   const [userId, setUserId] = useState(0);
+  const [showSettings, setShowSettings] = useState(false);
+  const [personality, setPersonality] = useState('friendly');
+  const [config, setConfig] = useState({
+    nickname: "AiVana",
+    nationality: "Filipino",
+    love_language: "Quality time",
+    user_mbti: "INFJ",
+    user_zodiac: "Virgo",
+    call_sign: "Mahal",
+    gender: "Female"
+  })
 
   useEffect(() => {
     if (!localStorage.getItem("userId")) {
@@ -75,6 +111,12 @@ export default function Home() {
       setUserId(randomId);
     }
   }, [])
+
+  const handleValueChange = (field, value) => {
+    const _config = Object.assign({}, config);
+    _config[field] = value;
+    setConfig(_config);
+  };
 
   const sendMessage = async () => {
     if (!input.trim()) return;
@@ -86,7 +128,7 @@ export default function Home() {
     fetch('https://ai-assistant-fawn-nine.vercel.app/api/ask_api', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ user_id: userId, question: input }),
+      body: JSON.stringify({ user_id: userId, question: input, config }),
     }).then(async (res) => {
       console.log(res);
       res.json().then((data) => {
@@ -95,10 +137,108 @@ export default function Home() {
     })
   };
 
+  const nationalities = [
+    { text: 'Filipino', value: 'Filipino' },
+    { text: 'Japanese', value: 'Japanese' },
+    { text: 'Korean', value: 'Korean' },
+    { text: 'American', value: 'American' },
+    { text: 'Chinese', value: 'Chinese' }
+  ]
+
+  const loveLanguages = [
+    { text: 'Word of affirmation', value: 'Filipino' },
+    { text: 'Quality time', value: 'Quality time' },
+    { text: 'Physical touch', value: 'Physical touch' },
+    { text: 'Acts of service', value: 'Acts of service' },
+    { text: 'Receiving gifts', value: 'Receiving gifts' }
+  ]
+
+  const zodiacSigns = [
+    { text: 'Aries', value: 'Aries' },
+    { text: 'Taurus', value: 'Taurus' },
+    { text: 'Gemini', value: 'Gemini' },
+    { text: 'Cancer', value: 'Cancer' },
+    { text: 'Leo', value: 'Leo' },
+    { text: 'Virgo', value: 'Virgo' },
+    { text: 'Libra', value: 'Libra' },
+    { text: 'Scorpio', value: 'Scorpio' },
+    { text: 'Sagittarius', value: 'Sagittarius' },
+    { text: 'Capricorn', value: 'Capricorn' },
+    { text: 'Aquarius', value: 'Aquarius' },
+    { text: 'Pisces', value: 'Pisces' }
+  ];
+
+  const mbtiTypes = [
+    { text: 'ISTJ - The Inspector', value: 'ISTJ' },
+    { text: 'ISFJ - The Protector', value: 'ISFJ' },
+    { text: 'INFJ - The Advocate', value: 'INFJ' },
+    { text: 'INTJ - The Architect', value: 'INTJ' },
+    { text: 'ISTP - The Virtuoso', value: 'ISTP' },
+    { text: 'ISFP - The Adventurer', value: 'ISFP' },
+    { text: 'INFP - The Mediator', value: 'INFP' },
+    { text: 'INTP - The Thinker', value: 'INTP' },
+    { text: 'ESTP - The Persuader', value: 'ESTP' },
+    { text: 'ESFP - The Performer', value: 'ESFP' },
+    { text: 'ENFP - The Campaigner', value: 'ENFP' },
+    { text: 'ENTP - The Debater', value: 'ENTP' },
+    { text: 'ESTJ - The Director', value: 'ESTJ' },
+    { text: 'ESFJ - The Consul', value: 'ESFJ' },
+    { text: 'ENFJ - The Protagonist', value: 'ENFJ' },
+    { text: 'ENTJ - The Commander', value: 'ENTJ' }
+  ];
+
+  const genders = [
+    { text: 'Male', value: 'Male' },
+    { text: 'Female', value: 'Female' }
+  ]
+
   return (
     <>
       <div style={styles.container}>
-        <h1 style={styles.title}>Your AiVana 1.0</h1>
+        <div style={styles.header}>
+          <h1 style={{ margin: 0 }}>Ur AiVana</h1>
+          <button onClick={() => setShowSettings(!showSettings)} style={styles.settingsButton}>
+            ⚙️
+          </button>
+        </div>
+
+        {showSettings && (
+          <div style={styles.settingsPanel}>
+            <h3>Settings</h3>
+            <br />
+            <label>Nickname</label> &nbsp;
+            <InputText value={config?.nickname} onChange={(e) => handleValueChange('nickname', e.target.value)} />
+            <br />
+            <br />
+            <label>Call Sign</label> &nbsp;
+            <InputText value={config?.call_sign} onChange={(e) => handleValueChange('call_sign', e.target.value)} />
+            <br />
+            <br />
+            <label>Nationality</label> &nbsp;
+            <Dropdown className={styles.selection} value={config?.nationality} options={nationalities} optionLabel='text' optionValue='value'
+              onChange={(e) => handleValueChange('nationality', e.value)} /> &nbsp;
+            <label>Partner Gender</label> &nbsp;
+            <Dropdown className={styles.selection} value={config?.gender} options={genders} optionLabel='text' optionValue='value'
+              onChange={(e) => handleValueChange('gender', e.value)} /> &nbsp;
+            <br />
+            <br />
+            <label>Love Language</label> &nbsp;
+            <Dropdown className={styles.selection} value={config?.love_language} options={loveLanguages} optionLabel='text' optionValue='value'
+              onChange={(e) => handleValueChange('love_language', e.value)} /> &nbsp;
+            <br />
+            <br />
+            <label>Your MBTI</label> &nbsp;
+            <Dropdown className={styles.selection} value={config?.user_mbti} options={mbtiTypes} optionLabel='text' optionValue='value'
+              onChange={(e) => handleValueChange('user_mbti', e.value)} />
+            <br />
+            <br />
+            <label>Your Zodiac</label> &nbsp;
+            <Dropdown className={styles.selection} value={config?.user_zodiac} options={zodiacSigns} optionLabel='text' optionValue='value'
+              onChange={(e) => handleValueChange('user_zodiac', e.value)} /> &nbsp;
+
+          </div>
+        )}
+
         <div style={styles.chatBox}>
           {messages.map((msg, idx) => (
             <div
@@ -110,10 +250,11 @@ export default function Home() {
                 color: msg.role === 'user' ? '#fff' : '#000',
               }}
             >
-              <span>{msg.content}</span>
+              {msg.content}
             </div>
           ))}
         </div>
+
         <div style={styles.inputContainer}>
           <input
             type="text"
